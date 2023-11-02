27d6da76793e84a868ffe3cdc5b012cffff9f1f5
Juanlozano11 <Juanes.lozano2003@gmail.com>
2023-11-01 21:24:37 -0700

    full page design
    missing the data

--- Webpage/app.py
+++ Webpage/app.py
@@ -1,53 +1,44 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a-very-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # You can change this path to your preferred location
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 db = SQLAlchemy(app)
 
-class ContactForm(FlaskForm):
-    name = StringField('Name')
-    email = StringField('Email')
-    message = TextAreaField('Message')
-    submit = SubmitField('Submit')
-
+# Define your Message model
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), nullable=False, unique=True)
-    message = db.Column(db.Text, nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), nullable=False)
+    message_text = db.Column(db.Text, nullable=False)
 
-    def __repr__(self):
-        return f"Message('{self.name}', '{self.email}')"
+# Define your ContactForm
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    message = TextAreaField('Message', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-@app.route('/')
+# Define your index route
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = ContactForm()
-    return render_template('index.html', form=form)
-
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    form = ContactForm()
-    
     if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-        message_text = form.message.data
-        
-        message = Message(name=name, email=email, message=message_text)
-        db.session.add(message)
+        new_message = Message(name=form.name.data, email=form.email.data, message_text=form.message.data)
+        db.session.add(new_message)
         db.session.commit()
-        
-        flash('Your message has been sent successfully!', 'success')
+        flash("Your message has been sent successfully!", "success")
         return redirect(url_for('index'))
-    
     return render_template('index.html', form=form)
 
+# Create the database tables within the app context
+with app.app_context():
+    db.create_all()
+
+# Run the app
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
